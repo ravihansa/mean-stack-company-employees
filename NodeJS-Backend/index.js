ac88c1f1aad702aa9c4e1ef4f5b34d2dfe15edc9
ravihansa <ravihansad@gmail.com>
2019-07-13 20:22:27 +0530

    Create node backend

---
+++ NodeJS-Backend/index.js
@@ -0,0 +1,30 @@
+require('./config/config');
+require('./config/db');
+require('./config/passportConfig');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+
+const rtsIndex = require('./routes/index.router');
+
+var app = express();
+
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use(passport.initialize());
+
+app.use('/api', rtsIndex);
+
+// for error handle
+app.use((err, req, res, next) => {
+    if (err.name === 'ValidationError') {
+        var valErrors = [];
+        Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
+        res.status(422).send(valErrors)
+    }
+});
+
+app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
